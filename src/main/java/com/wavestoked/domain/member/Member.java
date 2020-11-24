6206603a28d9fc6d8e52e5680eb76b7170711993
Woo-Park <wp503@nyu.edu>
2020-11-24 19:54:40 +0900

    manyToOne

---
+++ src/main/java/com/wavestoked/domain/member/Member.java
@@ -0,0 +1,80 @@
+package com.wavestoked.domain.member;
+
+
+import com.wavestoked.domain.team.Team;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@SequenceGenerator(
+        name = "MEMBER_ID_GENERATOR",
+        sequenceName = "memberId", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1,
+        allocationSize = 1
+)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_ID_GENERATOR")
+    @NotNull
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="TEAM_ID")
+    private Team team;
+
+}
+
+
+public void testing() {
+
+
+    Team team1 = new Team('blah', 'blah');
+
+    Member member1 = new Member('blah', 'blah');
+
+    Member member2 = new Member('blah', 'blah');
+
+    member1.setTeam(team1);
+    member2.setTeam(team1);
+
+    /*
+       toString의 recursion을 주의하자 -> json api중에 막는방법이 있다
+
+     * 하지만 team getMembers하면 null로 나온다 그래서
+
+     setTeam을 override해야하는데
+     setTeam(team) {
+        if(team.getMembers() != null) {
+            team.getMembers() // remove -> this.member
+
+        }
+        그다음에
+        this.team = team;
+        team.getMembers().add(this) // this member
+     }
+
+     이렇게하면 member에서도 team -> set read 둘다되고, team 에서는 get/ read만 된다
+     *
+     */
+    /*
+    * member 가 owner of this relationship
+    * member가 set 와 get을 더 많이하기 때문에
+    * member가 set을 통해 수시로 team을 바꿀수있기때문에
+    *
+    * 그래서 member에서 joinColumn() Team의 id
+    *
+    * Team쪽에서는 mappedBy()
+    *
+    *
+    * */
+
+
+}
